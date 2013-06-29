580f76ff39706ac29b4871d81d00e387f6593644
Farhan Khan <fkhan.1337@gmail.com>
2013-06-29 23:56:50 +0530

    Added comments

--- js/node-hello-world.js
+++ js/node-hello-world.js
@@ -1,9 +1,12 @@
+/* Import the http module */
 var http = require('http');
 
+/* Create http server, which prints "Hello, World!" and listen on port 8080 */
 http.createServer(function(request, response) {
     response.writeHead(200);
     response.write("Hello, World!");
     response.end();
 }).listen(8080);
 
+/* Print message to console to indicate a running server */
 console.log("Listening on port 8080...");
